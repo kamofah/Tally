a4d561710ef681f71fdb075ab8955967eeb506ee
Kwaku Amofah-Boafo <boafo819@gmail.com>
2026-06-20 21:22:56 -0400

    Refactor app structure and styles: updated title in index.html, removed unused CSS from App.css, and created Home component with corresponding styles.

---
+++ src/pages/Home.tsx
@@ -0,0 +1,24 @@
+import './Home.css'
+
+const Home = () => {
+  return (
+    <div className="container">
+        <div className="header">
+        <h1 className="title">Tally</h1>
+        <p className="description">Every letter has a price. The challenge is finding the word that's worth it.</p>
+        </div>
+        <div className="buttons">
+        <button className="button">Log in</button>
+        <button className="button button-filled">Play</button>
+        </div>
+        <div className="footer">
+        <p className="date">June 18, 2026</p>
+        <p className="number">Puzzle No. 1</p>
+        <p className="built">Built by Kwaku Amofah-Boafo</p>
+        <p className="copyright">© Tally</p>
+        </div>
+    </div>
+  )
+}
+
+export default Home
