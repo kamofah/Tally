7f9091f8dbd1c1451cf7d5b5aea2efe45d9d6c42
Kwaku Amofah-Boafo <boafo819@gmail.com>
2026-06-25 16:55:03 -0400

    Update routing in App component: replace Login route with Auth route to streamline authentication process.

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,17 @@
-
-import './App.css'
-import Home from './pages/Home'
-import Game from './pages/Game'
-import { Routes, Route } from 'react-router'
-import Login from './pages/Login'
-
+import "./App.css";
+import Home from "./pages/Home";
+import Game from "./pages/Game";
+import { Routes, Route } from "react-router";
+import Auth from "./pages/Auth";
 
 function App() {
-
   return (
     <Routes>
       <Route index element={<Home />} />
       <Route path="/game" element={<Game />} />
-      <Route path="/login" element={<Login />} />
+      <Route path="/auth" element={<Auth />} />
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
